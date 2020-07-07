b7067a27fea54538f8841d1553bd6017d6f1ec21
Ryan Viegas <rjkviegas@gmail.com>
2020-07-07 16:17:47 +0100

    added feature and tests so release_bike method returns a working Bike Object

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,4 +4,9 @@ describe DockingStation do
     it 'instance of DockingStation responds to release_bike method' do
         expect(DockingStation.new.respond_to?:release_bike).to eq true
     end
+
+    it 'calling release_bike on an instance of DockingStation returns instance of Bike' do
+        expect(DockingStation.new.release_bike.is_a?(Bike)).to eq true
+        expect(DockingStation.new.release_bike.working?).to eq true
+    end
 end
