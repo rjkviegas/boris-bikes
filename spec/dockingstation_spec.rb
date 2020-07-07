bc767b7869e180722f3d66b97451546e1e82809c
Ryan Viegas <rjkviegas@gmail.com>
2020-07-07 15:34:55 +0100

    updated dockingstation_spec.rb to test whether instances of DockingStation class respond_to?:release_bikes

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,3 +1,7 @@
 require 'dockingstation.rb'
+
 describe DockingStation do
+    it 'instance of DockingStation responds to release_bike method' do
+        expect(DockingStation.new.respond_to?:release_bike).to eq true
+    end
 end
